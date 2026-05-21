97118e0a588910ba0b630f3494a08339dcfe1481
Fadhil2806 <fadhil.pp200@gmail.com>
2026-05-22 00:18:54 +0700

    push setelah pull

--- mobile-react/config.js
+++ mobile-react/config.js
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-const API_URL = 'http://192.168.100.9:8000/api';
-=======
 const API_URL = 'http://192.168.100.8:8000/api';
->>>>>>> Stashed changes
-
 export default API_URL;
